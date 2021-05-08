c3681ef31273b46f5af6948855a907b034b77059
nenlep <nenlep12345@gmail.com>
2021-05-08 12:50:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import logo from './images/logo.svg';
+import mockup from "./images/illustration-mockups.svg";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo}></img>
       </header>
+      <main>
+        <img src={mockup}></img>
+        <div className="text">
+          <h2>Build the community your fans will love</h2>
+          <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </p>
+          <button>Register</button>
+        </div>
+        
+      </main>
+      <footer>
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-instagram"></i>
+      </footer>
     </div>
   );
 }
